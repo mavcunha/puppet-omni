d791a177cddefe91233d4e67b5605390429dee0c
Adam Stortz <astortz@redstonecontentsolutions.com>
2013-09-05 09:46:10 -0500

    reverted to appdmg_eula

--- manifests/plan.pp
+++ manifests/plan.pp
@@ -5,7 +5,7 @@
 #   include omni::plan
 class omni::plan {
   package { 'OmniPlan':
-    provider => 'appdmg',
+    provider => 'appdmg_eula',
     source   => 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.6/OmniPlan-2.2.4.dmg'
   }
 }
