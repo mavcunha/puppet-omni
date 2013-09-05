32f5552e0d1a4237e4c3cdeb9c53ce659c972424
Adam Stortz <astortz@redstonecontentsolutions.com>
2013-09-05 09:40:21 -0500

    switched to appdmg

--- manifests/plan.pp
+++ manifests/plan.pp
@@ -5,7 +5,7 @@
 #   include omni::plan
 class omni::plan {
   package { 'OmniPlan':
-    provider => 'appdmg_eula',
+    provider => 'appdmg',
     source   => 'http://www.omnigroup.com/download/latest/omniplan'
   }
 }
