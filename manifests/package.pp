f57f4cf5224f98f6a915c226a04ee2689deaf643
Marco Valtas <mavcunha@gmail.com>
2013-12-30 20:23:20 -0200

    Last style warning fixed

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,7 +3,7 @@
 # Installs OmniGroup packages through appdmg_eula provider
 #
 define omni::package($appversion, $osxversion, $appname = $title) {
-  package { "${appname}":
+  package { $appname:
     provider => 'appdmg_eula',
     source   => "http://downloads2.omnigroup.com/software/MacOSX/${osxversion}/${title}-${appversion}.dmg",
   }
