aa469d91f5936383a1e67dd6ca3ec4c71c83297f
Marco Valtas <mavcunha@gmail.com>
2014-01-16 10:30:29 -0300

    Updated to Outliner 4.0

--- spec/classes/omni_outliner_spec.rb
+++ spec/classes/omni_outliner_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'omni::outliner' do
   it do
     should contain_package('OmniOutliner').with({
-      :source    => 'http://downloads2.omnigroup.com/software/MacOSX/10.4/OmniOutliner-3.10.6.dmg',
+      :source    => 'http://downloads2.omnigroup.com/software/MacOSX/10.9/OmniOutliner-4.0.dmg',
       :provider	 => 'appdmg_eula'
     })
   end
